c45ed7820caaef843f9a656fbfda11f76f071de7
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-06 21:10:05 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -10,23 +10,6 @@ module Concerns
     def find_or_create_by_name(name)
       self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
     end
-
-    def all
-      @@all
-    end
-
-    def destroy_all
-      @@all.clear
-    end
-
-    def save
-      @@all << self
-      self
-    end
-
-    def create(name)
-      self.new(name).save
-    end
   end
 end
 
